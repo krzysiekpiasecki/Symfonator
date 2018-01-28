4f89a1deae53219b211dbb58f7d42231f4db868c
Krzysztof Piasecki <krzysiekpiasecki@gmail.com>
2018-01-28 14:51:54 +0100

    Set test envoriment

--- README.md
+++ README.md
@@ -1,12 +1,16 @@
 # Symfonator
-
 Welcome to Symfonator - Responsive Bootstrap 4 admin application based on the foundation of Symfony and Adminator HTML5 template!
 Preview of this application is available here [https://symfonator.herokuapp.com/admin](https://symfonator.herokuapp.com/admin).
+
+<br/>
 
 <a href="https://symfonator.herokuapp.com/admin">
 <img src="https://raw.githubusercontent.com/krzysiekpiasecki/Symfonator/master/src/assets/static/images/preview2.png" alt="Symfonator preview">
 </a>
 
+<br/>
+
+<br/>
 <br/>
 
 <a href="https://symfonator.herokuapp.com/admin">
---
+++ tests/Tests/Controller/AdminControllerTest.php
@@ -0,0 +1,53 @@
+<?php
+namespace App;
+
+use App\Entity\User;
+use Symfony\Bundle\FrameworkBundle\Client;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\BrowserKit\Cookie;
+use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
+
+
+class AdminControllerTest extends WebTestCase
+{
+    /**
+     * Test if application redirect to login page
+     */
+    public function testRedirectToLoginPage() {
+        $client = static::createClient();
+        $client->request('GET', '/admin');
+        $this->assertEquals(302, $client->getResponse()->getStatusCode());
+        $client->followRedirect();
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        $this->assertTrue($client->getCrawler()->filter("h4")->first()->text() === "Login");
+    }
+
+
+    /**
+     * @dataProvider listURL
+     * @param $url
+     */
+    public function testSmoke($url) {
+        $client = $this->logIn(static::createClient());
+        $client->request('GET', $url);
+        $this->assertEquals(302, $client->getResponse()->getStatusCode());
+        $this->markTestSkipped('Not implemented yet');
+    }
+
+    /**
+     * Provide data for {@link testSmoke}.
+     *
+     * @return \Generator
+     */
+    public function listURL() {
+        yield ['admin/compose'];
+    }
+
+    /**
+     * @param Client $client
+     * @return Client
+     */
+    private function logIn(Client $client): Client {
+        return $client;
+    }
+}
